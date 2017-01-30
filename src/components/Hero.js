7bd2624712d119cd5a4335a4ada2bfe13a12aa5f
kylem038 <misenckj@gmail.com>
2017-01-30 11:21:04 -0700

    Animation success!

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TweenMax, staggerFrom, Power2 } from 'gsap';
 
-const Hero = () => {
-  return(
-    <section className='home-page'>
-      <a id="about"></a>
-      <video className='video' autoPlay muted loop>
-        <source src="https://d1235ca2z646oc.cloudfront.net/videos/processed/536/273557707.mp4.mp4" type="video/mp4"></source>
-      </video>
-      <div className='value-prop'>
-        <h2><span>Hi, I'm Kyle</span></h2>
-        <h2><span>Front-End Developer & Software Engineer</span></h2>
-        <h2><span>Located In Denver, CO</span></h2>
-      </div>
-    </section>
-  );
+class Hero extends Component {
+
+  componentDidMount() {
+    this.gsapAnimation();
+  }
+
+  gsapAnimation(){
+    const e1 = this.one;
+    const e2 = this.two;
+    const e3 = this.three;
+
+    TweenMax.staggerFrom(
+      [e1, e2, e3],
+      0.7,
+      {
+        opacity: 0,
+        y: 50,
+        delay: 0.5,
+        ease: Power2.easeOut },
+      0.12)
+  }
+
+  render(){
+    return(
+      <section className='home-page'>
+        <a id="about"></a>
+        <video className='video' autoPlay muted loop>
+          <source src="https://d1235ca2z646oc.cloudfront.net/videos/processed/536/273557707.mp4.mp4" type="video/mp4"></source>
+        </video>
+        <div className='value-prop'>
+          <h2 ref={(c) => { this.one = c }}><span>Hi, I'm Kyle</span></h2>
+          <h2 ref={(c) => { this.two = c }}><span>Front-End Developer & Software Engineer</span></h2>
+          <h2 ref={(c) => { this.three = c }}><span>Located In Denver, CO</span></h2>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default Hero;
