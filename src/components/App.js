25c8f4ffb035065cc493eec99166e8b9c979c0cb
kylem038 <misenckj@gmail.com>
2017-01-30 09:43:57 -0700

    footer rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Nav from './Nav';
 import Hero from './Hero';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <Hero />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+    <footer>
+      <h2>Learn More About Me</h2>
+      <ul>
+        <li><a href="https://www.facebook.com/profile.php?id=7722680" target="_blank">
+          <img src="./images/facebook.svg" role="presentation" /></a></li>
+        <li><a href="https://www.instagram.com/kylemisencik/" target="_blank">
+          <img src="./images/instagram.svg" role="presentation" /></a></li>
+        <li><a href="https://www.linkedin.com/in/kylemisencik"  target="_blank">
+          <img src="./images/linkedin.svg" role="presentation" /></a></li>
+      </ul>
+    </footer>
+  )
+};
+
+export default Footer;
